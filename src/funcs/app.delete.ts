b50ac33a5e44c3801490d4e0e6b009b64413fa86
Finley Sherwood <me@itsf.in>
2022-09-15 12:46:21 +1000

    Create func to delete app connections

--- src/funcs/app.delete.ts
+++ src/funcs/app.delete.ts
@@ -1,4 +1,5 @@
 import Func, { HttpStatus } from "../models/Func"
+import { UserProperties } from "../types/user"
 
 /**
  * Remove an app from a parent. Called when an app is removed.
@@ -14,6 +15,47 @@ import Func, { HttpStatus } from "../models/Func"
  */
 export default class extends Func {
   public async run() {
-    return this.respond(HttpStatus.NotImplemented)
+    // Validate route
+    const parentId: string = this.context.bindingData.parentId
+    const appId: string | undefined = this.context.bindingData.appId
+
+    if (!appId)
+      return this.respond(HttpStatus.BadRequest, {
+        message: "Missing app ID in route"
+      })
+
+    // Check that the user is a child of the parent
+    if (!this.user) return this.respond(HttpStatus.Unauthorized)
+
+    const child = await this.query<Vertex<UserProperties, "user"> | undefined>(
+      `
+        g.V('${parentId}')
+          .hasLabel('user')
+        .outE('hasChild')
+          .where(
+            inV()
+              .has('userId', '${this.user.userId}')
+          )
+        .inV()
+      `
+    ).then(res => res._items[0])
+
+    if (!child) return this.respond(HttpStatus.Forbidden)
+
+    // Delete app connection
+    await this.query(`
+      g.V()
+        .hasLabel('user')
+        .has('userId', '${parentId}')
+      .outE('hasApp')
+        .where(
+          inV()
+            .has('appId', '${appId}')
+        )
+        .drop()
+    `)
+
+    // Respond to func call
+    return this.respond(HttpStatus.NoContent)
   }
 }
