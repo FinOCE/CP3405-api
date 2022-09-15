f936ecbda7f929cec5b15d27a49a7aaa6de21788
Finley Sherwood <me@itsf.in>
2022-09-15 11:52:05 +1000

    Create func to add app to parent

--- src/funcs/app.put.ts
+++ src/funcs/app.put.ts
@@ -1,10 +1,15 @@
 import Func, { HttpStatus } from "../models/Func"
+import fetch from "node-fetch"
+import { load } from "cheerio"
+import { UserProperties } from "../types/user"
 
 /**
  * Add an app to a parent. Called when an app is sent
  *
  * Route: PUT /users/{parentId}/apps/{appId}
- * Body: TBD
+ * Body: {
+ *  message: string | undefined
+ * }
  *
  * Possible responses:
  * - Unauthorized: User is not logged in - No data
@@ -14,6 +19,84 @@ import Func, { HttpStatus } from "../models/Func"
  */
 export default class extends Func {
   public async run() {
-    return this.respond(HttpStatus.NotImplemented)
+    // Validate route
+    const parentId: string = this.context.bindingData.parentId
+    const appId: string | undefined = this.context.bindingData.appId
+
+    if (!appId)
+      return this.respond(HttpStatus.BadRequest, {
+        message: "Missing app ID in route"
+      })
+
+    const message: string | undefined = this.req.body?.message
+
+    // Check that the user is a child of the parent
+    if (!this.user) return this.respond(HttpStatus.Unauthorized)
+
+    const child = await this.query<Vertex<UserProperties, "user"> | undefined>(
+      `
+        g.V('${parentId}')
+          .hasLabel('user')
+        .outE('hasChild')
+          .where(
+            inV()
+              .has('userId', '${this.user.userId}')
+          )
+        .inV()
+      `
+    ).then(res => res._items[0])
+
+    if (!child) return this.respond(HttpStatus.Forbidden)
+
+    // Check if app already exists in database
+    let app = await this.query<Vertex<App, "app"> | undefined>(
+      `
+        g.V('${appId}')
+          .hasLabel('app')
+      `
+    ).then(res => res._items[0])
+
+    // Add app to database if it doesn't exist
+    if (!app) {
+      // Scrape the play store for details
+      const html = await fetch(
+        `https://play.google.com/store/apps/details?id=${appId}`,
+        { method: "GET" }
+      ).then(res => res.text())
+
+      const $ = load(html)
+      const name = $(".Fd93Bb").find("span").text()
+      const creator = $(".Vbfug").find("span").text()
+      const iconUrl = $(".Mqg6jb").find("img").attr("src")
+
+      // Create app in database
+      app = await this.query<Vertex<App, "app">>(
+        `
+          g.addV('app')
+            .property('id', '${appId}')
+            .property('userId', 'NO_AFFILIATED_USER')
+            .property('appId', '${appId}')
+            .property('name', '${name}')
+            .property('creator', '${creator}')
+            .property('iconUrl', '${iconUrl}')
+        `
+      ).then(res => res._items[0])
+    }
+
+    // Link parent to app
+    await this.query(`
+      g.V('${parentId}')
+        .as('parent')
+      .V('${app!.id}')
+        .as('app')
+      .addE('hasApp')
+        .property('message', '${message}')
+        .property('timestamp', ${Date.now()})
+        .from('parent')
+        .to('app')
+    `)
+
+    // Respond to function call
+    return this.respond(HttpStatus.Ok, [{ type: "newApp", app }])
   }
 }
