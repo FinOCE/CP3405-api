de03e7a9f36a20d18a6dcf5b9ff3d1c3f852ae04
Finley Sherwood <me@itsf.in>
2022-09-28 15:48:29 +1000

    Fix parent adding itself

--- src/funcs/child.put.ts
+++ src/funcs/child.put.ts
@@ -64,7 +64,7 @@ export default class extends Func {
       g.V('${parentId}')
         .hasLabel('user')
         .as('parent')
-      .V('${this.user.userId}')
+      .V('${childId}')
         .hasLabel('user')
         .as('child')
       .addE('hasChild')
